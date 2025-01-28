ed0abfd092f4680f3c4195aa978eaefd83f46160
Cliford Jean-Louis <clifordj@kleefxdigital.com>
2025-01-28 03:01:13 -0300

    Updating Blog with main pages

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example: Toggle full blog content
+document.querySelectorAll('.btn-primary').forEach(button => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      alert('This is where the full blog post would appear!');
+    });
+  });
